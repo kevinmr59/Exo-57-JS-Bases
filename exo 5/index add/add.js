c70345f25c857db6719ee1d36c030dc3495f0e0f
kevinmr59 <kevinmura59610@gmail.com>
2021-11-15 14:49:54 +0100

    bases

---
+++ exo 5/index add/add.js
@@ -0,0 +1,4 @@
+let a = prompt("Entrez le premier nombre a virgule");
+let b = prompt("Entrer le deuxième nombre a virgule");
+let somme = parseInt(a) * parseInt(b);
+document.write("La somme de "+a+" et "+b+" égal à "+ somme)
---
+++ exo 6/add.js
@@ -0,0 +1,8 @@
+let a = prompt("Entrez votre âge svp");
+if(a<18)
+
+    alert('age non valide');
+if (a>=18)
+    alert('age valide')
+
+
